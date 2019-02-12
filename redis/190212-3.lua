7643120fca94ad8408d08e6f5a3eb55c0d6ba4bd
bestjiansong <yujiansong6@163.com>
2019-02-12 17:44:10 +0800

    redis+lua应用

---
+++ redis/190212-3.lua
@@ -0,0 +1,21 @@
+-- redis+lua集合去重
+local key=KEYS[1]
+local args=ARGV
+local result={}
+for m, n in ipairs(args) do
+	local existence = redis.call("sismember", key, n)
+	if(existence==1) then
+		table.insert(result, 1, n)
+	end
+end
+return result
+[[
+127.0.0.1:6379> smembers players
+1) "zhengbowen"
+2) "yujiansong"
+3) "liuyijie"
+4) "wanglaoer"
+5) "ouyanger"
+C:\Users\yujia>redis-cli --eval D:\practise\190212-3.lua players , wanglaoer laozheng abc
+1) "wanglaoer"
+]]
---
+++ redis/190212-4.lua
@@ -0,0 +1,40 @@
+-- redis+lua 找到hash中age小于指定值的所有数据
+local result={}
+local myperson=KEYS[1]
+local nums=ARGV[1]
+
+local myresult=redis.call("hkeys", myperson)
+
+for i, v in ipairs(myresult) do
+	local hval = redis.call("hget", myperson, v)
+	redis.log(redis.LOG_WARNING,hval)
+	if(tonumber(hval) < tonumber(nums)) then
+		table.insert(result, 1, v)
+	end
+end
+
+return result
+
+[[
+127.0.0.1:6379> hgetall ages
+ 1) "laozheng"
+ 2) "29"
+ 3) "wanglaoer"
+ 4) "38"
+ 5) "yujiansong"
+ 6) "28"
+ 7) "ouyanglaoer"
+ 8) "49"
+ 9) "liuyijie"
+10) "34"
+11) "huangzhen"
+12) "59"
+13) "xiaoming"
+14) "18"
+15) "chenzhi"
+16) "27"
+
+C:\Users\yujia>redis-cli --eval D:\practise\190212-4.lua ages , 28
+1) "xiaoming"
+2) "chenzhi"
+]]
