12cfb20e00420e7313eb8c0165935dbf1b03f8c7
于建松 <yujiansong6@163.com>
2019-03-22 21:19:42 +0800

    Yii2  ActionColumn+Button+Ajax功能应用
    Yii2  ActionColumn+Button+Ajax功能应用

---
+++ php/PostController.php
@@ -0,0 +1,13 @@
+<?php
+class PostController extends Controller
+{
+    // ...
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        if (!Yii::$app->request->isAjax) {
+            return $this->redirect(['index']);
+        }
+    }
+}
