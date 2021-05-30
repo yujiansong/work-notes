7688ea912548f7543f426e7898d57e6de1ebab01
于建松 <yujiansong6@163.com>
2021-05-30 16:40:49 +0800

    ReentrantLock使用

---
+++ Java/TestLock.java
@@ -0,0 +1,42 @@
+package com.jiansong.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestLock {
+    public static void main(String[] args) {
+        TestLock2 testLock2 = new TestLock2();
+        new Thread(testLock2).start();
+        new Thread(testLock2).start();
+        new Thread(testLock2).start();
+    }
+}
+
+class TestLock2 implements Runnable {
+    int ticketNum = 10;
+    private final ReentrantLock lock = new ReentrantLock();
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                lock.lock();
+                if (ticketNum > 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(ticketNum--);
+                } else {
+                    break;
+                }
+            } finally {
+                lock.unlock();
+            }
+
+        }
+    }
+
+
+
+
+}
