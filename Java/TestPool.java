c2944d692f53c0aee12b5d920e2ef76fea4d216f
于建松 <yujiansong6@163.com>
2021-05-31 23:01:47 +0800

    测试线程池

---
+++ Java/TestPool.java
@@ -0,0 +1,33 @@
+package com.jiansong.senior;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 测试线程池
+ * @author yujia
+ */
+public class TestPool {
+    public static void main(String[] args) {
+        // 1.创建服务，创建线程池
+        // newFixedThreadPool 参数为：创建线程池大小
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        // 执行
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+
+        // 关闭
+        service.shutdown();
+    }
+}
+
+class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
